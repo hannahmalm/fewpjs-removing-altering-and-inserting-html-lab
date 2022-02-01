9e5ce1eb504ee60f080eff8a171c4669bd814235
Hannah Malm <hannahemalm616@gmail.com>
2022-02-01 17:44:56 -0600

    commit tree 9b3d8168013150ff2ed51038855f8d48a17875b8

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your code here!
+// let element = document.createElement('div')
+// document.body.appendChild(element)
+
+// let ul = document.createElement('ul')
+
+// for (let i = 0; i < 3; i++) {
+//   let li = document.createElement('li')
+//   li.innerHTML = (i + 1).toString()
+//   ul.appendChild(li)
+// }
+
+// element.appendChild(ul)
+
+//after index.js is processed, no longer has DOM node main#main
+//expected <main id=main> to not exist
+document.getElementById('main').remove();
+
+
+//add a newHeader varaible that points to a node h1#victory with "YOUR-NAME is the champion" inside inner hmtl
+// var newHeader = document.createElement('h1#victory');
+var newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "YOUR-NAME is the champion"
+
+
+
